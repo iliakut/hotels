5493b01104d0ce2ac47ca39a8f01c9fc476954a4
ilia <ilyakut@gmail.com>
2021-04-29 20:01:55 +0300

    added HotelCard

--- src/components/LandingPage/HotelCard/HotelCard.tsx
+++ src/components/LandingPage/HotelCard/HotelCard.tsx
@@ -1,13 +1,21 @@
 import React, { FC } from 'react';
 import { HotelInterface } from '../../../lib/types/types';
 import style from './HotelCard.module.css';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 const HotelCard: FC<{ hotel: HotelInterface }> = (props) => {
+  const { t } = useTranslation();
+  const desctiption = props.hotel.description.length > 200
+    ? `${props.hotel.description.slice(0, 190)}...`
+    : props.hotel.description;
+
   return (
     <div className={style.card}>
       <h2>{props.hotel.name}</h2>
-      <h3>{props.hotel.description}</h3>
-      <h4>{props.hotel.rating}</h4>
+      <p className={style.description}>{desctiption}</p>
+      <p>{t('rating')} {props.hotel.rating}</p>
+      <Link to={'/'}>{t('hotelLink')}</Link>
     </div>
   );
 };
