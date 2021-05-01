9eed04661d962474f4e8d9c1428ce8ff41f86cfe
ilia <ilyakut@gmail.com>
2021-05-01 20:07:21 +0300

    room cards

--- src/components/HotelDetailPage/RoomCard/RoomCard.tsx
+++ src/components/HotelDetailPage/RoomCard/RoomCard.tsx
@@ -2,16 +2,27 @@ import React, { FC } from 'react';
 import { RoomInterface } from '../../../lib/types/types';
 import style from './RoomCard.module.css';
 import { useTranslation } from 'react-i18next';
+import StarsRating from '../../UI/StarsRating/StarsRating';
+import LinkStyled from '../../UI/LinkStyled/LinkStyled';
 
 const RoomCard: FC<{ room: RoomInterface }> = (props) => {
   const { t } = useTranslation();
+  const buttonText = props.room.available ? t('roomAvailable') : t('roomUnavailable');
 
   return (
     <div className={style.card}>
-      <h4>{props.room.name}</h4>
+      <div>
+        <p className={style.card__headerText}>{props.room.name}</p>
+        <h4 className={style.card__headerText}> {props.room.price_in_usd} {t('roomPrice')}</h4>
+      </div>
+      <div>
+        <StarsRating rating={props.room.rating}/>
+      </div>
+      <p>{t('maxOccupancy')}: <b>{props.room.max_occupancy}</b></p>
       <p>{props.room.description}</p>
-      <p><b>{t('maxOccupancy')}:</b> {props.room.max_occupancy}</p>
-      <p><b>{t('roomPrice')}:</b> {props.room.price_in_usd}</p>
+      <div className={style.card__button}>
+        <LinkStyled disabled={!props.room.available} to={'/'}>{buttonText}</LinkStyled>
+      </div>
     </div>
   );
 };
--- src/components/UI/LinkStyled/LinkStyled.tsx
+++ src/components/UI/LinkStyled/LinkStyled.tsx
@@ -2,13 +2,23 @@ import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import style from './LinkStyled.module.css';
 
-const LinkStyled: FC<{ className?: string, to: string }> = (props) => {
+const LinkStyled: FC<{ className?: string, to: string, disabled?: boolean }> = (props) => {
   return (
-    <Link
-      className={`${style.link} ${props.className ? props.className : ''}`}
-      to={props.to}>
-      {props.children}
-    </Link>
+    <>
+      {
+        props.disabled
+          ?
+          <button disabled={props.disabled} className={`${style.link} ${style.disabled} ${props.className ? props.className : ''}`}>
+            {props.children}
+          </button>
+          :
+          <Link
+            className={`${style.link} ${props.className ? props.className : ''}`}
+            to={props.to}>
+            {props.children}
+          </Link>
+      }
+    </>
   );
 };
 
